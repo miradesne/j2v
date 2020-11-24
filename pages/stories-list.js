e16475314bcd841c49607279a46be8218c8e46a8
Mira Chen <mirasc25@gmail.com>
2020-11-24 02:15:37 -0600

    load WP posts from graphql

--- pages/stories-list.js
+++ pages/stories-list.js
@@ -3,18 +3,19 @@ import Link from 'next/link'
 import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
+import { getAllPostsForHome } from '../lib/posts-graphql'
 import utilStyles from '../styles/utils.module.css'
 import storyCardStyles from '../components/story-card.module.scss'
 
-export default function StoriesList({ allPostsData }) {
+export default function StoriesList({ localPosts, wordPressPosts: {edges} }) {
   return (
     <Layout home={false}>
       <Head>…</Head>
       <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Lcoal Markdown Posts</h2>
         <div className={utilStyles.cardList}>
-          {allPostsData.map(({ id, date, title }) => (
+          {localPosts.map(({ id, date, title }) => (
             <div className={storyCardStyles.card} key={id}>
               <div className={storyCardStyles.meta} >
                 <div className={storyCardStyles.photo}>
@@ -27,8 +28,34 @@ export default function StoriesList({ allPostsData }) {
                 </Link>
                 <br />
                 <small className={utilStyles.lightText}>
-                  <Date dateString={date} />
+                  Date: <Date dateString={date} />
                 </small>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>WordPress Posts</h2>
+        <div className={utilStyles.cardList}>
+          {edges.map(({ node }) => (
+            <div className={storyCardStyles.card} key={node.slug}>
+              <div className={storyCardStyles.meta} >
+                <div className={storyCardStyles.photo}>
+                  <img src={ node.featuredImage.node.sourceUrl } />
+                </div>
+              </div>
+              <div className={storyCardStyles.description}>
+                <Link href={`/posts/${node.slug}`}>
+                    <a>{node.title}</a>
+                </Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  Date: <Date dateString={node.date} />
+                </small>
+                <div className={storyCardStyles.excerpt}>
+                  {node.excerpt.replace(/(<([^>]+)>)/gi, "")}
+                </div>
               </div>
             </div>
           ))}
@@ -39,10 +66,12 @@ export default function StoriesList({ allPostsData }) {
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const localPosts = getSortedPostsData()
+  const wordPressPosts = await getAllPostsForHome()
   return {
     props: {
-      allPostsData
+      localPosts,
+      wordPressPosts
     }
   }
 }
