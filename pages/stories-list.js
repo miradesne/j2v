fd76545519423565b194f6739384c3447dd17e12
Mira Chen <mirasc25@gmail.com>
2020-11-24 15:22:04 -0600

    add latest stories background

--- pages/stories-list.js
+++ pages/stories-list.js
@@ -6,61 +6,74 @@ import { getSortedPostsData } from '../lib/posts'
 import { getAllPostsForHome } from '../lib/posts-graphql'
 import utilStyles from '../styles/utils.module.css'
 import storyCardStyles from '../components/story-card.module.scss'
+import storyListPageStyles from '../components/storylistpage.module.css'
+
+function LocalPostsSection({ posts }) {
+  return (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Local Markdown Posts</h2>
+      <div className={utilStyles.cardList}>
+        {posts.map(({ id, date, title }) => (
+          <div className={storyCardStyles.card} key={id}>
+            <div className={storyCardStyles.meta} >
+              <div className={storyCardStyles.photo}>
+                <img src="https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg" />
+              </div>
+            </div>
+            <div className={storyCardStyles.description}>
+              <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                Date: <Date dateString={date} />
+              </small>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+function WordPressPosts({ posts }) {
+  return (
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Our Stories</h2>
+      <div className={utilStyles.cardList}>
+        {posts.map(({ node }) => (
+          <div className={storyCardStyles.card} key={node.slug}>
+            <div className={storyCardStyles.meta} >
+              <div className={storyCardStyles.photo}>
+                <img src={ node.featuredImage.node.sourceUrl } />
+              </div>
+            </div>
+            <div className={storyCardStyles.description}>
+              <Link href={`/posts/${node.slug}`}>
+                  <a>{node.title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                Date: <Date dateString={node.date} />
+              </small>
+              <div className={storyCardStyles.excerpt}>
+                {node.excerpt.replace(/(<([^>]+)>)/gi, "")}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
 
 export default function StoriesList({ localPosts, wordPressPosts: {edges} }) {
   return (
     <Layout home={false}>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Lcoal Markdown Posts</h2>
-        <div className={utilStyles.cardList}>
-          {localPosts.map(({ id, date, title }) => (
-            <div className={storyCardStyles.card} key={id}>
-              <div className={storyCardStyles.meta} >
-                <div className={storyCardStyles.photo}>
-                  <img src="https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg" />
-                </div>
-              </div>
-              <div className={storyCardStyles.description}>
-                <Link href={`/posts/${id}`}>
-                    <a>{title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  Date: <Date dateString={date} />
-                </small>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>WordPress Posts</h2>
-        <div className={utilStyles.cardList}>
-          {edges.map(({ node }) => (
-            <div className={storyCardStyles.card} key={node.slug}>
-              <div className={storyCardStyles.meta} >
-                <div className={storyCardStyles.photo}>
-                  <img src={ node.featuredImage.node.sourceUrl } />
-                </div>
-              </div>
-              <div className={storyCardStyles.description}>
-                <Link href={`/posts/${node.slug}`}>
-                    <a>{node.title}</a>
-                </Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  Date: <Date dateString={node.date} />
-                </small>
-                <div className={storyCardStyles.excerpt}>
-                  {node.excerpt.replace(/(<([^>]+)>)/gi, "")}
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+      <Head>Stories List</Head>
+      <div className={`${utilStyles.latestStoriesBackground} ${storyListPageStyles.latestStoriesBackground}`}>
+      </div>
+      <WordPressPosts posts={edges} />
     </Layout>
   )
 }
