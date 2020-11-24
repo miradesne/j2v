0f62cc7b694d326f0440ab734684f401c782039c
adhit <s.pradhitya@gmail.com>
2020-11-24 16:31:54 -0500

    Actually don't need the toplevel div

--- components/layout.js
+++ components/layout.js
@@ -10,63 +10,61 @@ export const siteTitle = 'Next.js J2V Website'
 
 export default function Layout({ children, home }) {
   return (
-    <div className={styles.toplevel}>
-      <div className={styles.container}>
-        <Head>
-          <link
-            rel="preload"
-            href="/fonts/Roobert/Roobert-Regular.otf"
-            as="font"
-            crossOrigin=""
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Learn how to build a personal website using Next.js"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.now.sh/${encodeURI(
-              siteTitle
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={siteTitle} />
-          <meta name="twitter:card" content="summary_large_image" />
-        </Head>
-        <header className={styles.header}>
-          {home ? (
+    <div className={styles.container}>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Roobert/Roobert-Regular.otf"
+          as="font"
+          crossOrigin=""
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="https://journeytovalley.org/wp-content/uploads/2020/09/logo.png"
+              className={`${styles.headerImage}`}
+              alt={name}
+            />
+            <HeaderNavBar />
+          </>
+        ) : (
             <>
-              <img
-                src="https://journeytovalley.org/wp-content/uploads/2020/09/logo.png"
-                className={`${styles.headerImage}`}
-                alt={name}
-              />
-              <HeaderNavBar />
-            </>
-          ) : (
-              <>
-              <Link href="/">
-                <a>
-                  <img
-                    src="https://journeytovalley.org/wp-content/uploads/2020/09/logo.png"
-                    className={`${styles.headerImage}`}
-                    alt={name}
-                  />
-                </a>
-              </Link>
-              <HeaderNavBar />
-              </>
-          )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div className={styles.backToHome}>
             <Link href="/">
-              <a>← Back to home</a>
+              <a>
+                <img
+                  src="https://journeytovalley.org/wp-content/uploads/2020/09/logo.png"
+                  className={`${styles.headerImage}`}
+                  alt={name}
+                />
+              </a>
             </Link>
-          </div>
+            <HeaderNavBar />
+            </>
         )}
-      </div>
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
