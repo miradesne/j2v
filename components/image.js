354ca616505df39b3164cb6f43751cb516e0fe64
Mira Chen <mirasc25@gmail.com>
2020-11-28 14:50:47 -0600

    fix

--- components/image.js
+++ components/image.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import storyCardStyles from "./story-card.module.scss";
 
 export default function Img({ src }) {
   return <Image layout="fill" src={src} className={storyCardStyles.img} />;
