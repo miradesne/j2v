feac0da5f2d00c992858ed02bb15d8b1d8e799ae
Mira Chen <mirasc25@gmail.com>
2020-11-28 14:57:32 -0600

    change preload to true

--- components/image.js
+++ components/image.js
@@ -2,5 +2,12 @@ import Image from "next/image";
 import storyCardStyles from "./story-card.module.scss";
 
 export default function Img({ src }) {
-  return <Image layout="fill" src={src} className={storyCardStyles.img} />;
+  return (
+    <Image
+      layout="fill"
+      src={src}
+      className={storyCardStyles.img}
+      priority={true}
+    />
+  );
 }
