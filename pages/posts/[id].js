69bb87d4f135eb24358aa87f001b32aa2dc4bd1d
novrandobilly <novrandobilly@gmail.com>
2021-07-31 14:09:25 +0700

    Add Go to top button single story page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
@@ -7,10 +8,25 @@ import CommentSection from '../../components/single-story/comment-section';
 import OtherStories from '../../components/single-story/other-stories';
 import styles from './single-article.module.css';
 import { getAllPostsForHome } from '../../lib/posts-graphql';
+import Scroll from 'react-scroll';
+const scroll = Scroll.animateScroll;
 
 export default function Post({ postData, wordPressPosts }) {
+  const [gototopStyle, setGototopStyle] = useState('');
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 750) setGototopStyle(styles['gototop-show']);
+      if (window.scrollY < 750) setGototopStyle('');
+    });
+  }, [gototopStyle]);
   return (
     <Layout home={false}>
+      <img
+        onClick={() => scroll.scrollToTop({ duration: 1000 })}
+        className={`${styles.gototop} ${gototopStyle}`}
+        src='/images/single-story/gototop.svg'
+        alt='go to top'></img>
       <Head>
         <title>{postData.title}</title>
       </Head>
